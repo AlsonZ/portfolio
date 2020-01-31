bdebb88a16b80e77144de1de86883328eec577a2
AlsonZ <48876218+AlsonZ@users.noreply.github.com>
2020-02-01 06:46:06 +1100

    change project card design

--- src/projects/project-card.js
+++ src/projects/project-card.js
@@ -4,20 +4,15 @@ import './project-card.css';
 
 function ProjectCard(props) {
 
-  const onClickDemo = () => {
-    
-  }
-  const onClickGithub = () => {
-    
-  }
-
   return (
     <div className="project-card-container">
       <img src={image} alt="image" className="project-card-image"></img>
-      <h2 className={"project-card-title"}>{props.title}</h2>
-      <div className="project-card-actions">
-        <button onClick={onClickDemo} className="project-card-button">Demo</button>
-        <button onClick={onClickGithub} className="project-card-button">Github</button>
+      <div className="project-card-overlay">
+        <h2 className="project-card-title">{props.title}</h2>
+        <div className="project-card-actions">
+          <a href="#" className="project-card-button project-demo">Demo</a>
+          <a href="#" className="project-card-button project-github">Github</a>
+        </div>
       </div>
     </div>
   );
