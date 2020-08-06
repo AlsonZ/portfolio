7f08186a20d5fd98f78fcb21ec02b41cfe890699
AlsonZ <48876218+AlsonZ@users.noreply.github.com>
2020-08-07 02:29:20 +1000

    add new images and project descriptions

--- src/projects/projects.js
+++ src/projects/projects.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ProjectCard from './project-card';
 import './projects.css';
-import portfolioImage from '../imgs/project-images/portfolio-image.PNG';
-import todolistImage from '../imgs/project-images/todolist-image.PNG';
+import portfolioImage from '../imgs/project-images/portfolio.png';
+import todolistImage from '../imgs/project-images/todolist.png';
 import weatherBundleImage from '../imgs/project-images/weatherbundle-image.PNG';
 import conwayImage from '../imgs/project-images/conway-image.PNG';
 import exchangeRateImage from '../imgs/project-images/exchange-rate-image.PNG';
-import dragAndDropImage from '../imgs/project-images/drag-and-drop-image.PNG';
+import dragAndDropImage from '../imgs/project-images/draganddrop.png';
 import imagepalImage from '../imgs/project-images/imagepal.PNG';
-import upartsImage from '../imgs/project-images/uparts.PNG';
+import upartsImage from '../imgs/project-images/uparts.png';
 
 function Projects() {
 
@@ -16,49 +16,51 @@ function Projects() {
     {
       title: "UParts",
       image: upartsImage,
-      description: "A storefront webpage selling computer hardware",
+      description: "Using HTML, CSS and Javascript to create a online store selling computer hardware.",
       webLink: "",
       githubLink: "https://github.com/AlsonZ/ComputerPartsStore"
     },
     {
       title: "Imagepal",
       image: imagepalImage,
-      description: "Imagepal is a forum where people use images to communicate",
+      description: "Imagepal is a forum where people use images to communicate." +
+      "\n This project was based upon my university assignment project idea. " +
+      "\n I redesigned and redeveloped the entire project using the MERN stack. It uses node.js, express in the backend with MongoDB as the database and React in the frontend.",
       webLink: "https://imagepal.herokuapp.com/",
       githubLink: "https://github.com/AlsonZ/ImagePal"
     },
     {
       title: "Portfolio",
       image: portfolioImage,
-      description: "My portfolio to display what I have learnt and can accomplish",
+      description: "My portfolio to display what I have learnt and the projects i have created.",
       webLink: "https://alsonz.github.io/portfolio/",
       githubLink: "https://github.com/AlsonZ/portfolio"
     },
     {
       title: "Todo List",
       image: todolistImage,
-      description: "A Fullstack Todolist website with login, REST and CRUD operations",
+      description: "A Fullstack Todolist website with login, REST and CRUD operations.",
       webLink: "https://quiet-fjord-80903.herokuapp.com/",
       githubLink: "https://github.com/AlsonZ/fullstack-todolist/"
     },
     {
       title: "Weather Bundle",
       image: weatherBundleImage,
-      description: "Showing Weather information for Sydney by web scraping 3 sites",
+      description: "Showing Weather information for Sydney by web scraping 3 sites.",
       webLink: "https://secure-coast-19528.herokuapp.com/",
       githubLink: "https://github.com/AlsonZ/Weather-Bundle"
     },
     {
       title: "Conway's Game of Life",
       image: conwayImage,
-      description: "A cellular automaton devised by the British mathematician John Horton Conway in 1970 ",
+      description: "A cellular automaton devised by the British mathematician John Horton Conway in 1970. ",
       webLink: "https://alsonz.github.io/game-of-life/",
       githubLink: "https://github.com/AlsonZ/game-of-life"
     },
     {
       title: "Exchange Rate Calculator",
       image: exchangeRateImage,
-      description: "Using the https://exchangeratesapi.io/ API to get the most recent exchange rates",
+      description: "Using the https://exchangeratesapi.io/ API to get the most recent exchange rates.",
       webLink: "https://alsonz.github.io/exchange-rate-calculator/",
       githubLink: "https://github.com/AlsonZ/exchange-rate-calculator"
     },
