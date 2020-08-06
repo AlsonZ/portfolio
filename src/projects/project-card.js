bc7ca9b492b291f550d88046a17a2eaae155910a
AlsonZ <48876218+AlsonZ@users.noreply.github.com>
2020-08-06 20:04:33 +1000

    remove old description and actions from project card overlay

--- src/projects/project-card.js
+++ src/projects/project-card.js
@@ -26,12 +26,13 @@ function ProjectCard({project}) {
   return (
     <div className="project-card-container">
       <img src={project.image} alt="" className="project-card-image"></img>
-      <div className="project-card-overlay">
-        <h2 className="project-card-title">{project.title}</h2>
-        <p className="project-card-desc" onClick={()=>{handleProjectOverlay()}}>{project.description}</p>
+      <div className="project-card-overlay" onClick={()=>{handleProjectOverlay()}}>
+        <i className="fas fa-search-plus icon-visible"></i>
+        {/* <p className="project-card-desc" >{project.description}</p> */}
         <div className="project-card-actions">
-          <a href={project.webLink} className="project-card-button project-demo">Website</a>
-          <a href={project.githubLink} className="project-card-button project-github">Github</a>
+          <h2 className="project-card-title">{project.title}</h2>
+          {/* <a href={project.webLink} className="project-card-button project-demo">Website</a> */}
+          {/* <a href={project.githubLink} className="project-card-button project-github">Github</a> */}
         </div>
       </div>
     </div>
